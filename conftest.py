8c80f817fa9cd53e25a65a7248e1aa15bccd4cc2
Екатерина Сычева <katherinesycheva@gmail.com>
2020-12-15 15:25:31 +0300

    Some changes for better understanding

--- conftest.py
+++ conftest.py
@@ -2,6 +2,7 @@ import pytest
 from selenium import webdriver
 
 
+# parameter language
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default="ru",
                      help="Choose language: en, ru, fr, es etc.")
@@ -15,5 +16,6 @@ def browser(request):
     options.add_argument('--lang=' + language)
     browser = webdriver.Chrome(options=options)
     yield browser
-    print("\nquit browser..")
+    print("\nquit chrome browser..")
     browser.quit()
+
--- test_items.py
+++ test_items.py
@@ -3,6 +3,8 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_add_to_basket_button_is_exist(browser):
     browser.get(link)
-    button = browser.find_element_by_css_selector("#add_to_basket_form > button")
-    print(button.text)
-    assert button is not None, "Button not found!"
+    button_basket = browser.find_element_by_css_selector("#add_to_basket_form > button")
+    # if button not None
+    assert button_basket is not None, "Button not found!"
+
+
